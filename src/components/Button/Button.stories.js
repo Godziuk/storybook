15c812effff1c8e41d4bdf69371dcdbf58dfae89
Godziuk <grzesiekgodziuk@gmail.com>
2020-10-22 15:00:15 +0200

    Add args promp children

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -4,10 +4,34 @@ import Button from './Button';
 //w stories lewy panel
 export default{
     title: 'Form/Control/Button',
-    component: Button
+    component: Button,
+    args: {
+        children: 'Button' //działa wszystkich children i nadaje text na button ten sam czyli 'Button'
+    }
+
 }
 //eksport różnych story dla buttona
 export const Primary = () => <Button variant='primary'>Primary</Button>
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Success</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button>
+
+const Template = args => <Button {...args } />
+
+export const PrimaryA = Template.bind({})
+PrimaryA.args = {
+    variant: 'primary',
+    children: 'Primary Args'
+}
+
+export const LongPrimaryA = Template.bind({})
+LongPrimaryA.args = {
+    ...PrimaryA.args,
+    //children: 'Long Primary Ags'
+}
+
+export const SecondaryA = Template.bind({})
+SecondaryA.args = {
+    variant: 'secondary',
+    //children: 'Secondary Args'
+}
